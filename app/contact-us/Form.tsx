35d5ac41db993ab7f731b2226ad97e4f473a3954
jephthahdominic <stdominicjephthah63@gmail.com>
2025-11-03 22:39:52 +0100

    worked on changes

--- app/contact-us/Form.tsx
+++ app/contact-us/Form.tsx
@@ -41,7 +41,7 @@ export const Form = () => {
         setStatus("Sending...");
 
         try {
-            const res = await fetch("/api/contact", {
+            const res = await fetch("api/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
